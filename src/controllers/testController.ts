f13dbe361762305b1a79c15ee505931c021f5908
NgYujinBenjamin <benjaminng.2017@sis.smu.edu.sg>
2022-11-14 03:31:24 +0800

    added basic logic

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,11 +1,18 @@
+import { dataMall } from "../api/datamall";
 import { Controller, Route, Get } from "tsoa";
-// import { env } from 'process';
 
 @Route("users")
 export class BaseController extends Controller {
   @Get("")
   public async getUser(): Promise<any> {
-    console.log(process.env.API_KEY);
+    try{
+      let dao = new dataMall;
+      let response = await dao.test();
+      console.log(response.json());
+    } catch (e){
+      console.log(e);
+    }
+
     return "hello";
   }
 }
