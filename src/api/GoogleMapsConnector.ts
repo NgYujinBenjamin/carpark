1ed26f3d008041f1a85212d8e784d16e8487d83d
NgYujinBenjamin <benjaminng.2017@sis.smu.edu.sg>
2022-11-19 22:38:07 +0800

    minor changes

--- src/api/GoogleMapsConnector.ts
+++ src/api/GoogleMapsConnector.ts
@@ -5,6 +5,17 @@ export default class GoogleMapsConnector {
   baseUrl: string = ``;
 
   accountKey: string = process.env.MAPS_API_KEY || '';
+  mapId: string = process.env.MAPS_ID || '';
+
+  apiOptions = {
+    "apiKey": this.accountKey
+  };
+
+  mapOptions = {
+    "zoom": 18,
+    "center": { lat: 35.6594945, lng: 139.6999859 },
+    "mapId": this.mapId
+  }
 
   public async getPlaceholder(): Promise<Response> {
     // const url = ``;
