50b5873a96ae81849c420086754eb3757f87d543
NgYujinBenjamin <benjaminng.2017@sis.smu.edu.sg>
2022-11-21 18:14:41 +0800

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ const app: Application = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+// if (process.env.OPERATIONAL_ENV === 'dev'){
+app.use((_req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `http://localhost:3000`);
+  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept, Authorization`);
+  next();
+});
+// }
+
 app.use(
   '/docs',
   swaggerUi.serve,
