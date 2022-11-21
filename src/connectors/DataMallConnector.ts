1bf45d96e6300649f9d112a85bcb67414482f8f9
NgYujinBenjamin <benjaminng.2017@sis.smu.edu.sg>
2022-11-21 21:20:00 +0800

    added correct start command and refactored connectors

---
+++ src/connectors/DataMallConnector.ts
@@ -0,0 +1,26 @@
+import {Response} from 'node-fetch';
+import {GETDataMallAPI} from './utils';
+
+export default class DataMallConnector {
+  baseUrl: string = `http://datamall2.mytransport.sg/ltaodataservice/`;
+
+  accountKey: string = process.env.DATAMALL_API_KEY || '';
+
+  // public async getBusRoutes(skipValue: number): Promise<Response> {
+  //   const url = `${this.baseUrl}BusRoutes?$skip=${skipValue.toString}`;
+  //   const response = await GETDataMallAPI(url, this.accountKey);
+  //   if (!response.ok) {
+  //     throw new Error(`HTTP error! status: ${response.status}`);
+  //   }
+  //   return response.json() as unknown as Response;
+  // }
+
+  public async getCarparkAvailability(skipValue: number): Promise<Response> {
+    const url = `${this.baseUrl}CarParkAvailabilityv2?$skip=${skipValue.toString()}`;
+    const response = await GETDataMallAPI(url, this.accountKey);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json() as unknown as Response;
+  }
+}
